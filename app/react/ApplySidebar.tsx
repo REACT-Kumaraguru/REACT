6cbefc1f631232f98a1d9bd0afba5bc7d1487bdd
react <reactkumaraguru@gmail.com>
2025-04-17 05:54:03 +0530

    Force rebuild: touch ApplySidebar

---
+++ app/react/ApplySidebar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+type Props = {
+  openPhase: (value: "explore" | "reflect" | "solve" | "apply") => void;
+};
+
+export default function ApplySidebar({ openPhase }: Props) {
+  return (
+    <div className="w-full lg:w-[240px]">
+      <button onClick={() => openPhase("apply")}>Open Apply Phase</button>
+    </div>
+  );
+}
+// Force rebuild
