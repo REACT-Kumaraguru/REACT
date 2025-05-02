42c557f42a1159ca8ef5c471fc3cec218e4b8bb3
react <reactkumaraguru@gmail.com>
2025-05-03 00:02:07 +0530

    mobile responsive updated

--- app/react/page.tsx
+++ app/react/page.tsx
@@ -65,7 +65,7 @@ export default function ReactPage() {
             href="/select-user"
             className="inline-block bg-black text-white px-4 py-2 rounded hover:bg-gray-800 transition"
           >
-            Apply Now
+            <span className="text-white">Apply Now</span>
           </a>
         </aside>
       </div>
